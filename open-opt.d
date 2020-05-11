4ae16cc9f9dee64b29e7607b5c0c6fb3a00435d7
mariadias143 <marianovadias@gmail.com>
2020-05-11 22:28:09 +0100

    🥪

--- open-opt.d
+++ open-opt.d
@@ -20,8 +20,8 @@ syscall::openat*:return
 /strstr(copyinstr(self->path),"/etc") != 0/
 {
 	printf("%-6d %-16s %-6d %-6d %-64s %-8s", pid, execname, uid, gid,
-	    copyinstr(self->path), (self->path&O_WRONLY) ? "O_WRONLY" :
-	    (self->path&O_RDWR) ? "O_RDWR" : "O_RDONLY");
+	    copyinstr(self->path), (self->flag&O_WRONLY) ? "O_WRONLY" :
+	    (self->flag&O_RDWR) ? "O_RDWR" : "O_RDONLY");
 	printf("%-8s", self->flag&O_CREAT ? " | O_CREAT" : "");
 	printf("%-8s", self->flag&O_APPEND ? " | O_APPEND" : "");
 	printf("%d\n",arg1);
